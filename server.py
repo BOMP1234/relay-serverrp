649708f5634697cf4f79b283489c113a98f2c998
BOMP1234 <134557311+BOMP1234@users.noreply.github.com>
2026-06-26 02:46:08 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,24 @@
+import os
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+import uvicorn
+
+app = FastAPI()
+clients = []
+
+@app.websocket("/")  # A RELAY_URL-nek ehhez kell igazodnia
+async def relay(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # Az összes többi kliensnek továbbküldjük
+            for client in clients:
+                if client != websocket:
+                    await client.send_text(data)
+    except WebSocketDisconnect:
+        clients.remove(websocket)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
